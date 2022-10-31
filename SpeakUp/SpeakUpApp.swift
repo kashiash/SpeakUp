67bfc705b21c4d34bc0e2ef46b2822dff82630ee
Jacek Kosiński <kashiash@gmail.com>
2022-10-31 19:42:35 +0100

    Initial Commit

---
+++ SpeakUp/SpeakUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakUpApp.swift
+//  SpeakUp
+//
+//  Created by Jacek Kosinski U on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpeakUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
